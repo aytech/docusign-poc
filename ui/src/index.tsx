cdbdab2816da9b032e0eadf07296377133a996b4
Oleg Yapparov <oyapparov@gmail.com>
2020-11-25 14:14:57 +0100

    Add UI

---
+++ ui/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <h2>Happy coding :)</h2>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
